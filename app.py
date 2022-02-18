cc3c5da8de3192c3944db2498f635e0af92489d6
Dilan Madusanka <madmadusanka@gmail.com>
2022-02-18 06:44:39 +0530

    segmentation

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import base64
 from io import BytesIO
+import io
 from typing import NamedTuple
+from urllib import response
 from flask import Flask, render_template, request, url_for, jsonify
 import imageio
+from matplotlib.pyplot import clf
 from skimage.color import rgb2hsv
 import numpy as np
 import cv2 
@@ -17,30 +20,31 @@ class Request():
     def __init__(self, base64):
 
         self.base64 = base64
+  
+def toBase64(binary_file):
+    binary_file_data = binary_file
+    base64_encoded_data = base64.b64encode(binary_file_data)
+    base64_message = base64_encoded_data
+    return base64_message
 
 @app.route("/predict" ,methods = ['GET' , 'POST'])
 def hello():
     try:
         print("request.get_json()")
-
-        base64 = request.headers.get('base64')
-        # img = Image.open(BytesIO(base64.decodebytes(base64)))
-        print(base64)
-
-        base64_img_bytes = base64.encode('utf-8')
+        base64Image = request.headers.get('base64') 
+        base64_img_bytes = base64Image.encode('utf-8')
         with open('decoded_image.png', 'wb') as file_to_save:
+            print(file_to_save)
             decoded_image_data = base64.decodebytes(base64_img_bytes)
-            file_to_save.write(decoded_image_data)
-   
-        # with open("imageToSave.png", "wb") as fh:
-        #     fh.write(base64.b64decode(text))
-
-        im1 = imageio.imread("C:/newfolder/image.jpg")
-        leaf_hsv = rgb2hsv(im1)
+            file_to_save.write(decoded_image_data)  
+        image = Image.open(io.BytesIO(decoded_image_data))
+        image_np = np.array(image)
     
-
-        lower_mask = leaf_hsv[:,:,0] > 0.2 #refer to hue channel (in the colorbar)
-        upper_mask = leaf_hsv[:,:,0] < 0.5 #refer to transparency channel (in the colorbar)
+        im1 = image_np
+        leaf_hsv = rgb2hsv(im1)
+  
+        lower_mask = leaf_hsv[:,:,0] > 0.2 
+        upper_mask = leaf_hsv[:,:,0] < 0.5 
         saturation_mask = leaf_hsv[:,:,1] > 0.4 
     
         mask1 = upper_mask*lower_mask*saturation_mask
@@ -49,10 +53,11 @@ def hello():
         blue = im1[:,:,2]*mask1
         leaf_masked = np.dstack((red,green,blue))
         x1=cv2.fastNlMeansDenoisingColored(leaf_masked,None,10,10,7,21)
+        
 
 
-        lower_mask = leaf_hsv[:,:,0] > 0.0 #refer to hue channel (in the colorbar)
-        upper_mask = leaf_hsv[:,:,0] < 0.18 #refer to transparency channel (in the colorbar)
+        lower_mask = leaf_hsv[:,:,0] > 0.0 
+        upper_mask = leaf_hsv[:,:,0] < 0.18 
         saturation_mask = leaf_hsv[:,:,1] > 0
         mask2 =upper_mask*lower_mask*saturation_mask
         red = im1[:,:,0]*mask2
@@ -68,43 +73,65 @@ def hello():
         blue = im1[:,:,2]*mask3
         leaf_masked = np.dstack((red,green,blue))
         x3=cv2.fastNlMeansDenoisingColored(leaf_masked,None,10,10,7,21)
-        imageio.imsave("C:/newfolder/image1.jpg", x1)
-        imageio.imsave("C:/newfolder/image2.jpg", x2)
-        imageio.imsave("C:/newfolder/image3.jpg", x3)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+        black_pixels = np.where(
+            (x1[:, :, 0] == 0) & 
+            (x1[:, :, 1] == 0) & 
+            (x1[:, :, 2] == 0)
+            )
+        x1[black_pixels] = [255, 255, 255]
+        
+        black_pixels = np.where(
+            (x2[:, :, 0] == 0) & 
+            (x2[:, :, 1] == 0) & 
+            (x2[:, :, 2] == 0)
+            )
+        x2[black_pixels] = [255, 255, 255]
+        
+        black_pixels = np.where(
+            (x3[:, :, 0] == 0) & 
+            (x3[:, :, 2] == 0)
+            )
+        x3[black_pixels] = [255, 255, 255]
+        
+        imageio.imsave("withoutSymptom.jpg", x1)
+        imageio.imsave("symptom.jpg", x2)
+        imageio.imsave("leaf.jpg", x3)
 
         model = load_model('E:/User Document/my_model.h5')
-        image = Image.open('E:/User Document/processed/PotassiumHigh/PotassiumHigh44.jpg')
+        image = Image.open('symptom.jpg')
         image = image.resize((180, 180))
-        print("this")
-        print(image)
         image = np.array(image)
-        img_tensor = np.expand_dims(image, axis=0) 
-        print(model.predict(img_tensor))
+        img_tensor = np.expand_dims(image, axis=0)
+        prediction =  model.predict(img_tensor)
+        maxClass = max(prediction[0])
+        labelIndex =prediction[0].tolist().index(maxClass)
+
+        # ['NitrogenHigh', 'NitrogenLow', 'PhosphorusHigh', 'PhosphorusLow', 'PotassiumHigh', 'PotassiumLow']  
+
+
+        with open("symptom.jpg", "rb") as img_file:
+            b64_string = base64.b64encode(img_file.read())
+            symptom = b64_string.decode('utf-8')
+
+        with open("leaf.jpg", "rb") as img_file:
+            b64_string = base64.b64encode(img_file.read())
+            leaf = b64_string.decode('utf-8')
+
+        response = app.response_class(
+        response='succeed',
+        status=200,
+        mimetype='application/json')
+        response.headers.add('symptom',symptom)
+        response.headers.add('leaf',leaf)
+        response.headers.add('pred',labelIndex)
+        print(labelIndex)
+        return response
     except Exception as e:
         print(e)
-        return e
-    return 'fail'
+        return "failed"
+    return 'succeed'
     
-  
 
 
 
